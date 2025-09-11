c0609427cfd3b42b51a62230cb74087c3431f535
Jomana Mohammed <jomanamohammed816@gmail.com>
2025-09-11 21:51:35 +0300

    Implement code changes to enhance functionality and improve performance+added title

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ import arrorw from "../../assets/white-arrow.png";
 import { Link } from "react-scroll";
 import img1 from "../../assets/image.png";
 import img2 from "../../assets/image2.jpg";
-import img3 from "../../assets/image3.jpg";
+import img3 from "../../assets/image3.webp";
 
 const Hero = () => {
   const images = [img1, img2, img3];
@@ -32,13 +32,19 @@ const Hero = () => {
       ))}
 
       <div className="hero-content">
-        <h1>Welcome to FitLife Gym</h1>
-        <p>Transform your body, mind, and lifestyle with us.</p>
-        <Link to="programs" smooth={true} duration={500}>
-          <button className="btn">
+        <h1>Push Beyond Your Limits</h1>
+        <p>
+          At <strong>FitLife Gym</strong>, we are more than just a fitness center, we are a community 
+          committed to helping you achieve strength, confidence, and a healthier lifestyle. 
+          From personalized training programs to world-class equipment and expert guidance, 
+          everything you need to transform your body and mind is right here. 
+        </p>
+        
+          <button className="btn"><Link to="program" smooth={true} offset={-250} duration={500}>
             Get Started <img src={arrorw} alt="arrow" />
+            </Link>
           </button>
-        </Link>
+        
       </div>
     </div>
   );
